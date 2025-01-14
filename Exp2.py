22cab8e2198533b9f2cc696581001949808ca7ab
Git2025Pot <145266688+Git2025Pot@users.noreply.github.com>
2025-01-14 14:18:54 +0530

    Update Exp2.py

--- Exp2.py
+++ Exp2.py
@@ -1,6 +1,19 @@
 import pandas as pd
-import matplotlib as plt
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import LabelEncoder
 
-df = pd.read_csv('insurance.csv');
-print(df);
+df = pd.read_csv('https://raw.githubusercontent.com/Git2025Pot/Experiment2/refs/heads/main/insurance.csv');
+
+le = LabelEncoder()
+led = le.fit_transform(df['insu_status'])
+df['insu_status'] = led
+
+lst=['age','salary','insu_status']
+df = df[lst];
+
+x = df['age'];
+y = df['insu_status'];
+plt.scatter(x,y);
+plt.xlabel('age');
+plt.ylabel('Insurance status');
+plt.show();
